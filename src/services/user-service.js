80a85772906652b5aba6d6fad2667f31f76d328b
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2023-01-04 10:20:25 +0530

    Added jwt create and verify token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const UserRepository = require('../repository/user-reporitory');
 
 class UserService {
@@ -10,6 +12,51 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    async sigIn(email, plainPassword) {
+        const user = this.UserRepository.getByEmail(email);
+
+        const passwordMatch = this.checkPassword(plainPassword, user.password)
+
+        if (!passwordMatch) {
+            console.log("Password doesn't match");
+            throw { error: 'Incorrect password' };
+        }
+
+        const newJwt = this.createToken({ email: user.email, id: user.id });
+        return newJwt;
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1h' });
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation", error);
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword)
+        } catch (error) {
+            console.log("Something went wrong in password comparisation", error);
             throw error;
         }
     }
